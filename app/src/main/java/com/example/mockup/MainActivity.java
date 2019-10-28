ad55093c521274030108c60b66fdb203d9e15f48
rosidanurf <rosidanurf.24@gmail.com>
2019-10-28 16:33:08 +0700

    16.30

--- app/src/main/java/com/example/mockup/MainActivity.java
+++ app/src/main/java/com/example/mockup/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mockup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,4 +12,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    Intent i = new 
 }
